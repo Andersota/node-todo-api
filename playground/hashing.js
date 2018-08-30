c9065841749dc2e64b063673012e4ec20a5e53fa
Jonny Anderson <Jonny@Rolf.local>
2018-08-30 15:50:00 -0500

    Hash Passord before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,6 @@
 const {SHA256} = require( 'crypto-js' );
-
 const jwt = require( 'jsonwebtoken' );
+const bcrypt = require( 'bcryptjs' );
 
 var message = 'I am jonny';
 var hash = SHA256( message ).toString();
@@ -25,3 +25,9 @@ console.log( token );
 var decoded = jwt.verify( token, 'salt string' );
 
 console.log( decoded );
+
+bcrypt.genSalt( 10, ( err, salt ) => {
+	bcrypt.hash( password, salt, ( err, hash ) => {
+		
+	});
+});
